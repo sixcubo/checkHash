673cce9cec5e83b3f87af406d93fdb4611d1d69a
666 <1339713363@qq.com>
2020-09-15 21:21:57 +0800

    Finish the SHA1 command line version.

---
+++ checkSHA1.py
@@ -0,0 +1,86 @@
+import hashlib
+import sys
+import os.path
+
+recordsName = r"SHA1.records"
+
+def getFileMD5(filePath):
+    with open(filePath, "rb") as fp:
+        md5obj = hashlib.md5()
+        md5obj.update(fp.read())
+        fileMD5 = md5obj.hexdigest()
+        return fileMD5
+
+
+def isIgnore(filePath):
+    fileType = ['records', 'jpeg', 'png' ,'jpg' ]
+    if filePath.split('.')[-1] in fileType:
+        print('ignore ' + filePath)
+        return True
+
+    return False
+
+
+
+
+# 获取文件的SHA1值
+def getFileSHA1(filePath):
+    with open(filePath, "rb") as fp:
+        SHA1Obj = hashlib.sha1()
+        SHA1Obj.update(fp.read())
+        fileSHA1 = SHA1Obj.hexdigest()
+        return fileSHA1
+
+
+# 检查目录树下文件的SHA1值
+def checkRecords(basepath):
+    # 遍历目录树
+    for dirpath, dirnames, filenames in os.walk(basepath):
+        # 打开records
+        with open(dirpath + '\\' + recordsName, "r+") as fp:
+            # 读取records中的文件名和SHA1值
+            records = fp.readlines()
+            flienNameOfRecord = []
+            flienSHA1OfRecord = []
+            for i in range(len(records)):
+                records[i] = records[i].split('\t')
+                flienNameOfRecord.insert(i, records[i][0])
+                flienSHA1OfRecord.insert(i, records[i][1])
+
+            # 遍历当前目录
+            for filename in filenames:
+                if not isIgnore(dirpath + "\\" + filename):
+                    fileSHA1 = getFileSHA1(dirpath + "\\" + filename)
+                    # 若文件已存在于records，检查与记录是否相同
+                    if filename in flienNameOfRecord:
+                        idx = flienNameOfRecord.index(filename)
+                        if fileSHA1 == flienSHA1OfRecord[idx]:
+                            print('SHA1 equle\t' + dirpath + '\\' + filename)
+                        else:
+                            print('SHA1 not equle\t' + dirpath + '\\' + filename)
+                            flienSHA1OfRecord[idx] = fileSHA1
+                    # 若文件不存在于records，插入记录
+                    else:
+                        print('SHA1 not exist\t' + dirpath + '\\' + filename)
+                        flienNameOfRecord.insert(len(flienNameOfRecord), filename)
+                        flienSHA1OfRecord.insert(len(flienSHA1OfRecord), fileSHA1)
+
+            # 写入记录
+            for i in range(len(flienNameOfRecord)):
+                fp.write(flienNameOfRecord[i] + '\t' + flienSHA1OfRecord[i] + "\t\n")
+
+
+# 遍历目录树，在每个目录下生成records
+def createRecords(basepath):
+    for dirpath, dirnames, filenames in os.walk(basepath):
+        try:
+            fp = open(dirpath + '\\' + recordsName, "x")
+        except FileExistsError:
+            print(dirpath + '\\' + recordsName + r" is EXISTED")
+        else:
+            fp.close()
+
+
+if "__main__" == __name__:
+    createRecords(r"D:\testhash")
+    checkRecords(r"D:\testhash")
